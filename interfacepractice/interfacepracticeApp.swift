826355512c9731d86fc1b07a508cea3a5de1e4e0
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-04-18 12:57:15 -0400

    Initial Commit

---
+++ interfacepractice/interfacepracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  interfacepracticeApp.swift
+//  interfacepractice
+//
+//  Created by Michelle Elias Flores on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct interfacepracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
